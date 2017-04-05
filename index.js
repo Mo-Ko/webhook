366a91becfdcee01154fb950a4b89e9259640eb2
Moko <mohsin.kokab@accenture.com>
2017-04-05 15:41:42 +0100

    Test 5

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ restService.use(bodyParser.json());
 
 restService.post('/', function(req, res) {
     var speech = req.body.result && req.body.result.parameters && req.body.result.parameters.echoText ? req.body.result.parameters.echoText : "Seems like some problem. Speak again."
-    console.log("Abc");
+    console.log(req.body.result.metadata.intentName);
 	return res.json({
         speech: speech,
         displayText: speech,
