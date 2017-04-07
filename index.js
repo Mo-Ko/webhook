104a65e89a7dc1ecf732f8b44504f57da8b5fcfc
Moko <mohsin.kokab@accenture.com>
2017-04-07 15:45:01 +0100

    made changes

--- index.js
+++ index.js
@@ -12,19 +12,18 @@ restService.use(bodyParser.urlencoded({
 restService.use(bodyParser.json());
 
 restService.post('/', function(req, res) {
-     console.log("IntentName" + req.body.result.metadata.intentNam);
+     console.log("IntentName: " + req.body.result.metadata.intentName);
   
  //   var speech = req.body.result && req.body.result.parameters && req.body.result.parameters.echoText ? req.body.result.parameters.echoText : "Seems like some problem. Speak again."
     
-    console.log("Command: " + req.body.result.parameters.play);
     
-    if(req.body.result.metadata.intentNam === "Play-Intent"){
+    if(req.body.result.metadata.intentName === "Play-Intent"){
         return res.json({
             speech: "Ok Playing the Video",
             displayText: "Ok Playing the Video",
             source: 'webhook-echo-sample'
         });    
-    }else if(req.body.result.metadata.intentNam === "Stop-Intent"){
+    }else if(req.body.result.metadata.intentName === "Stop-Intent"){
         return res.json({
             speech: "Ok Stoping the Video",
             displayText: "Ok Stoping the Video",
